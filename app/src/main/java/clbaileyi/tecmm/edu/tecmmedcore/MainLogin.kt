2a35b4a2f2acbb348ba4acba215239d1156a3eeb
Erick Pacheco Barcenas <erick.pacheco.sc@itszapopan.edu.mx>
2018-04-30 18:18:49 -0500

    Inicio de pantallas de inicio

---
+++ app/src/main/java/clbaileyi/tecmm/edu/tecmmedcore/MainLogin.kt
@@ -0,0 +1,12 @@
+package clbaileyi.tecmm.edu.tecmmedcore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+}
